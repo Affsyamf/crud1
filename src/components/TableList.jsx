6a95c910015f4acd346491733586d008af52cbd7
Afif Syam Fauzi <afiffauzi2016@gmail.com>
2025-10-21 12:14:59 +0700

    icon action

--- src/components/TableList.jsx
+++ src/components/TableList.jsx
@@ -1,4 +1,27 @@
+import { ArrowPathIcon, TrashIcon } from '@heroicons/react/24/solid'
+
 export default function TableList() {
+   
+    const clients = [{
+        id: 1,
+        name: "Cy Ganderton",
+        job: "Quality Control Specialist",
+        favoriteColor: "Blue"
+    }, 
+    {
+    id: 2,
+        name: "Hart Hagerty",
+        job: "Desktop Support Technician",
+        favoriteColor: "Purple"
+    },
+    {
+    id: 3,
+        name: "Brice Swyre",
+        job: "Tax Accountant",
+        favoriteColor: "Red"
+    }
+]
+   
     return (
         <>
         <div className="overflow-x-auto">
@@ -10,30 +33,36 @@ export default function TableList() {
         <th>Name</th>
         <th>Job</th>
         <th>Favorite Color</th>
+        <th>Status</th>
+        <th>Action</th>
       </tr>
     </thead>
-    <tbody>
+    <tbody className="hover">
       {/* row 1 */}
-      <tr className="hover:bg-base-300">
-        <th>1</th>
-        <td>Cy Ganderton</td>
-        <td>Quality Control Specialist</td>
-        <td>Blue</td>
+
+      {clients.map((client) => (
+        <tr key={client.id} className="hover:bg-base-300">
+        <th> {client.id} </th>
+        <th>{client.name}</th>
+        <td>{client.job}</td>
+        <td>{client.favoriteColor}</td>
+        <td>
+            <button className={`btn rounded-full w-20 ${client.isactive ? 'btn-primary' : 'btn-outline btn-primary'}`}>
+            {client.isactive ? 'Active' : 'Inactive'}
+            </button>
+        </td>
+        <td>
+            <button>
+            <ArrowPathIcon  className="h-6 w-6 text-blue-500 hover:text-blue-700" />
+            </button>
+            <button>
+            <TrashIcon  className="h-6 w-6 mx-2 text-red-500 hover:text-red-700" />
+            </button>
+        </td>
       </tr>
-      {/* row 2 */}
-      <tr className="hover:bg-base-300">
-        <th>2</th>
-        <td>Hart Hagerty</td>
-        <td>Desktop Support Technician</td>
-        <td>Purple</td>
-      </tr>
-      {/* row 3 */}
-      <tr className="hover:bg-base-300">
-        <th>3</th>
-        <td>Brice Swyre</td>
-        <td>Tax Accountant</td>
-        <td>Red</td>
-      </tr>
+      ))}
+        
+     
     </tbody>
   </table>
 </div>
